42ac1bea7918a867f13896af2aebe399f50764d0
Thien Pham <chithien175@gmail.com>
2018-09-16 09:07:46 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,10 +13,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        //
-    }
+    
+public function boot()
+{
+    Schema::defaultStringLength(191);
+}
 
     /**
      * Register any application services.
